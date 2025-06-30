974752b392c0c847f92f1a26e24bff8b1ee8c743
gtang8 <74369627+gtang8@users.noreply.github.com>
2025-06-30 10:15:56 +0800

    Update RuleProviders.ts

--- src/config/RuleProviders.ts
+++ src/config/RuleProviders.ts
@@ -9,6 +9,10 @@ export const RuleProviders: RuleProvider[] = [
         value: "__DEFAULT"
     },
     {
+        key: "GT-Lite",
+        value: "https://raw.githubusercontent.com/gtang8/ACL4SSR/master/Clash/config/ACL4SSR_Mini.ini"
+    },
+    {
         key: "ACL_默认版",
         value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini"
     },
